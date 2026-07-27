ecd9fc6bd4edd8bd21d75ef056f7e358f8ebef59
Saaqure94 <86102026+Saaqure94@users.noreply.github.com>
2026-07-27 10:19:32 +1000

    Fix trapezoid integration method to trapz

--- test_fokker_planck.py
+++ test_fokker_planck.py
@@ -31,7 +31,7 @@ def test_finite_difference_matches_analytical_when_stable():
     res = FiniteDifferenceSolver(MU, SIGMA).solve(T, dx=0.02, r=0.4)
     assert res.stable
     p_exact = analytical_density(res.x, T, MU, SIGMA)
-    l2 = np.sqrt(np.trapezoid((res.p - p_exact) ** 2, res.x))
+    l2 = np.sqrt(np.trapz((res.p - p_exact) ** 2, res.x))
     assert l2 < 1e-3
 
 
